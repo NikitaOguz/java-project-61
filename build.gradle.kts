39379137e2b5ade72d027698679b7b3509f12d93
Никита Кобытев <nikitakobytev@mail.ru>
2025-10-16 23:44:35 +0700

    fix make run-dist

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,31 +3,41 @@ plugins {
     id("application")
     id("checkstyle")
 }
+
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 application {
-    // Указываем главный класс приложения
-    mainClass = ("hexlet.code.App")
+    mainClass.set("hexlet.code.App")
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 checkstyle {
     toolVersion = "10.12.4"
 }
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     add("checkstyle", "com.puppycrawl.tools:checkstyle:${checkstyle.toolVersion}")
 }
-tasks.withType<Checkstyle>().configureEach {
-    javaLauncher.set(javaToolchains.launcherFor {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    })
+
+tasks.withType<JavaCompile> {
+    options.release.set(17)
 }
+
 tasks.test {
     useJUnitPlatform()
 }
+
 
