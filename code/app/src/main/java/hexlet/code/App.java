b3ef7e5d1e68783fc737e8400d255d19d8766b2b
Никита Кобытев <nikitakobytev@mail.ru>
2025-10-25 01:21:13 +0700

    fix error

--- code/app/src/main/java/hexlet/code/App.java
+++ code/app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Engine;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class App {
     private static final Logger log = Logger.getLogger(App.class.getName());
@@ -18,50 +19,47 @@ public class App {
         0 - Exit""");
 
         int number = Input.scanner.nextInt();
+
         switch (number) {
-            case 1:
+            case 1 -> {
                 log.info("Your choice: 1");
                 Cli.selectname();
-                break;
-
-            case 2:
+            }
+            case 2 -> {
                 log.info("Your choice: 2");
                 Cli.selectname();
                 log.info("Answer 'yes' if the number is even, otherwise answer 'no'.");
                 Engine.generation();
-                break;
-
-            case 3:
+            }
+            case 3 -> {
                 log.info("Your choice: 3");
                 Cli.selectname();
                 Engine.expression();
-                break;
-
-            case 4:
+            }
+            case 4 -> {
                 log.info("Your choice: 4");
                 Cli.selectname();
                 Engine.nod();
-                break;
-
-            case 5:
+            }
+            case 5 -> {
                 log.info("Your choice: 5");
                 Cli.selectname();
                 Engine.progression();
-                break;
-
-            case 6:
+            }
+            case 6 -> {
                 log.info("Your choice: 6");
                 Cli.selectname();
                 Engine.prime();
-                break;
-
-            case 0:
+            }
+            case 0 -> {
                 return;
-
-            default:
-                log.warning("'yes' is wrong answer ;(. Correct answer was 'no'.\n" +
-                        "Let's try again, " + Cli.name);
+            }
+            default ->
+                    log.log(Level.WARNING,
+                            "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {0}",
+                            Cli.name);
         }
     }
 }
+
 
