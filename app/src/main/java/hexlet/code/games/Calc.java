a02a6df99c643ea168bc87777a4633b6921fcaa4
NikitOguz <140327196+NikitaOguz@users.noreply.github.com>
2025-11-04 23:33:24 +0700

    Update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -7,41 +7,33 @@ public class Calc {
     private static final String DESC = "What is the result of the expression?";
     private static final int ROUNDS_COUNT = 3;
     private static final int MAX_RANDOM = 100;
-    private static final int OPERATIONS_COUNT = 3;
+    private static final String[] OPERATIONS = {"+", "-", "*"};
 
     public static void calc() {
-
         String[][] rounds = new String[ROUNDS_COUNT][2];
 
         for (int i = 0; i < rounds.length; i++) {
             int first = Rand.RANDOM.nextInt(MAX_RANDOM);
             int second = Rand.RANDOM.nextInt(MAX_RANDOM);
-            int operation = Rand.RANDOM.nextInt(OPERATIONS_COUNT);
-            String question = first + " " + getOperator(operation) + " " + second;
-            String correctAnswer = String.valueOf(getCalc(first, second, operation));
+            int operationIndex = Rand.RANDOM.nextInt(OPERATIONS.length);
 
-            rounds[i][0] = question;
-            rounds[i][1] = correctAnswer;
+            String operator = OPERATIONS[operationIndex];
+            String question = first + " " + operator + " ";
+            int result = getCalc(first, second, operator);
+
+            rounds[i][0] = question + second;
+            rounds[i][1] = String.valueOf(result);
         }
 
         Engine.enginegame(DESC, rounds);
     }
 
-    private static int getCalc(int a, int b, int operation) {
-        return switch (operation) {
-            case 0 -> a + b;
-            case 1 -> a - b;
-            case 2 -> a * b;
-            default -> 0;
-        };
-    }
-
-    private static String getOperator(int operation) {
-        return switch (operation) {
-            case 0 -> "+";
-            case 1 -> "-";
-            case 2 -> "*";
-            default -> "?";
+    private static int getCalc(int a, int b, String operator) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            default -> throw new RuntimeException("Unknown operator: " + operator);
         };
     }
 }
