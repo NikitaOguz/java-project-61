94cbdbebd6f97b791bece87cf4ee158d3012849c
NikitOguz <140327196+NikitaOguz@users.noreply.github.com>
2025-11-04 23:21:47 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,6 @@ checkstyle {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     add("checkstyle", "com.puppycrawl.tools:checkstyle:${checkstyle.toolVersion}")
 }
 
